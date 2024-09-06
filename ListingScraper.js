eee29f0c17b7974ac93b0019e34c96d8cba57e6d
AbdullahFarooq123 <abdullahfarooq20172018@gmail.com>
2024-09-07 02:38:32 +0500

    Init

--- ListingScraper.js
+++ ListingScraper.js
@@ -3,4 +3,4 @@ setTimeout(() => {
     type: "LISTING_SCRAPED",
     listing: document.querySelector('div[class="sc-2940a84d-0 ifHyQT"]').outerHTML,
   });
-}, Math.floor(Math.random() * 3) + 3);
+}, Math.floor(Math.random() * (4000 - 2000 + 1)) + 2000);
